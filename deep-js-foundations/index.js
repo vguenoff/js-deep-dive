5fd6454cd7e77b957eb25f35f2af0bad52844547
vguenoff <vguenoff@gmail.com>
2022-07-08 10:16:00 +0300

    equality solution

--- deep-js-foundations/index.js
+++ deep-js-foundations/index.js
@@ -1,3 +1,4 @@
 // import './00-types/index'
 // import './exercises/types-exercises/object-is/ex'
-import './exercises/types-exercises/coercion/ex'
+// import './exercises/types-exercises/coercion/ex'
+import './exercises/types-exercises/equality/ex'
